4b1a07b9b3d193421fa947adf1fc770f6866e38c
David Palacio <dpalacio@uninorte.edu.co>
2008-12-23 16:06:31 -0500

    fixed lots of krap. builds a 486 sid image

---
+++ config/chroot_local-hooks/quitar_kdm.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+apt-get remove --purge --yes kdm
+
---
+++ config/chroot_local-includes/home/user/.kde4/Autostart/mensaje.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+kdialog --title 'KDE4 Live CD' --msgbox 'This is an unofficial KDE4.2 Live CD.\n\nIf you have trouble, please do not file bug reports on the Debian bugtracker. Instead, bug dpalacio on #debian-kde.'
